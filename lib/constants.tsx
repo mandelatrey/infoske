8187c56e4c762a300fc433a56ded9f63b6f9e3ca
Mandela Trevor <mandelatrevor@gmail.com>
2025-09-28 09:43:04 +0300

    Fixed the header nav that was not showing

--- lib/constants.tsx
+++ lib/constants.tsx
@@ -1,6 +1,6 @@
 export const NAV_ITEMS = [
-    { href: '/', label: 'Dashboard' },
-    { href: '/search', label: 'Search' },
+    { href: '/', title: 'Dashboard' },
+    { href: '/search', title: 'Search' },
     // { href: '/watchlist', label: 'Watchlist' },
 ];
 
